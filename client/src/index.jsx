595ec459be660b0337a635c5b938d5b85b5f632f
wootsaejao <wootsaejao@gmail.com>
2015-12-15 02:51:28 +0700

    add bootstrap, sass

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
+// bootstrap
+require('bootstrap/dist/css/bootstrap.min.css')
+jQuery = $ = window.jQuery = window.$ =  require('jquery/dist/jquery.min');
+require('bootstrap/dist/js/bootstrap.min')
+
+// custom style
+require("!style!css!sass!./style.scss")
+
+
 render(
   <App />,
   document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,13 @@ module.exports = {
   ],
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: ['react-hot', 'babel']
-      },
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      // for bootstrap.css
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?mimetype=application/octet-stream" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?mimetype=image/svg+xml" }
+      { test: /\.scss$/, loaders: ['style', 'sass'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(woff|woff2)$/, loader:'url?prefix=font/' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?mimetype=image/svg+xml' }
     ]
   },
   resolve: {
