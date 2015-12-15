e5700d33ca820706b414cf086ecaec6f007f879d
wootsaejao <wootsaejao@gmail.com>
2015-12-15 18:55:58 +0700

    eslint, react-bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,10 +3,13 @@ import React from 'react' // eslint-disable-line no-unused-vars
 import { render } from 'react-dom'
 import App from './components/App'
 
-// bootstrap
+// This section is for webpack asset loader
+// bootstrap css
 require('bootstrap/dist/css/bootstrap.min.css')
-jQuery = $ = window.jQuery = window.$ =  require('jquery/dist/jquery.min');
-require('bootstrap/dist/js/bootstrap.min')
+
+// // bootstrap js
+// jQuery = $ = window.jQuery = window.$ =  require('jquery/dist/jquery.min');
+// require('bootstrap/dist/js/bootstrap.min')
 
 // custom style
 require('!style!css!sass!./style.scss')
