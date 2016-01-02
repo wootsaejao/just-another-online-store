2be46af18afd18760d93684bebf0bb535a347ac7
wootsaejao <wootsaejao@gmail.com>
2016-01-03 00:06:26 +0700

    cleanup

--- server/src/server.js
+++ server/src/server.js
@@ -29,19 +29,21 @@ module.exports = (port) => {
         path: '/{param*}',
         handler: (request, reply) => {
           const param = request.params.param
+
+          // prevent accesing index.html file
           if (param === 'index.html') {
             reply(Boom.notFound())
+          }
+
+          if (process.env.NODE_ENV !== 'production') {
+            // proxy to webpack dev server
+            reply.proxy({
+              host: 'localhost',
+              port: port + 1,
+              protocol: 'http'
+            })
           } else {
-            if (process.env.NODE_ENV !== 'production') {
-              // proxy to webpack dev server
-              reply.proxy({
-                host: 'localhost',
-                port: port + 1,
-                protocol: 'http'
-              })
-            } else {
-              reply.file('./static/' + param)
-            }
+            reply.file('./static/' + param)
           }
         }
       })
