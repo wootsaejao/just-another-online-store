4549dd2f2ecebec369aac16e4466bb5dcc2d9aae
wootsaejao <wootsaejao@gmail.com>
2016-01-05 17:13:11 +0700

    add auth code from react-router example

---
+++ client/src/containers/Logout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logout = React.createClass({
+  componentDidMount() {
+    auth.logout()
+  },
+
+  render() {
+    return <p>You are now logged out</p>
+  }
+})
+
+export default Logout
