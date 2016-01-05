73ce7bdc9e0e76bc2d994e9f4e494d01edb3f064
wootsaejao <wootsaejao@gmail.com>
2016-01-06 02:23:11 +0700

    cleanup

--- client/src/actions/TimeActions.js
+++ client/src/actions/TimeActions.js
@@ -11,9 +11,10 @@ export function getTime(delay) {
     types: [GET_TIME_REQUEST, GET_TIME_SUCCESS, GET_TIME_FAILURE],
     promise: () => {
       return new Promise((resolve/*, reject*/) => {
-        // Just simulating an async request to a server via a setTimeout
         // We can do anything we want, and finish with resolve()
         // or handle error with reject()
+        
+        // Just simulating an async request to a server via a setTimeout
         setTimeout(() => {
           const d = new Date()
           const ms = ('000' + d.getMilliseconds()).slice(-3)
