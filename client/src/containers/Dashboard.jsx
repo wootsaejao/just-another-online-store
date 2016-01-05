c6b1b0465a32d5dfb444f2121c26f7f3c5ee5443
wootsaejao <wootsaejao@gmail.com>
2016-01-05 17:33:16 +0700

    typo

--- client/src/containers/Dashboard.jsx
+++ client/src/containers/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Table from '../components/Table'
 
-export default class HomePage extends Component {
+export default class Dashboard extends Component {
   render() {
     return (
       <div>
--- client/src/containers/NotFound.jsx
+++ client/src/containers/NotFound.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class HomePage extends Component {
+export default class NotFound extends Component {
   render() {
     return (
       <div>404 Not Found. I think you have lost.</div>
