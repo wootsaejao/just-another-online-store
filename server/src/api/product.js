b33b1bd25e05168ed8e76b544b6334b9ef7d3981
wootsaejao <wootsaejao@gmail.com>
2016-01-07 17:22:02 +0700

    add file product api

---
+++ server/src/api/product.js
@@ -0,0 +1,33 @@
+//
+// Product API
+//
+
+// import Boom from 'boom'
+//
+// export default [
+//   {
+//     method: 'POST',
+//     path: '/api/products',
+//     handler: handleCreateProduct
+//   },
+//   {
+//     method: 'GET',
+//     path: '/api/products',
+//     handler: handleGetProducts
+//   },
+//   {
+//     method: 'GET',
+//     path: '/api/products/{id}',
+//     handler: handleGetProductById
+//   },
+//   {
+//     method: 'PUT',
+//     path: '/api/products/{id}',
+//     handler: handleUpdateProduct
+//   },
+//   {
+//     method: 'DELETE',
+//     path: '/api/products/{id}',
+//     handler: handleDeleteProduct
+//   },
+// ]
