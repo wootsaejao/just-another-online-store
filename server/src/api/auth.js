a0fc5a53637108d7c55eb449c9341deefd5f5ae3
wootsaejao <wootsaejao@gmail.com>
2016-01-07 19:48:19 +0700

    add some code from example

--- server/src/api/auth.js
+++ server/src/api/auth.js
@@ -61,6 +61,61 @@ function handleLogout(request, reply) {
   })
 }
 
+// const login = function (request, reply) {
+//
+//     if (request.auth.isAuthenticated) {
+//         return reply.redirect('/');
+//     }
+//
+//     let message = '';
+//     let account = null;
+//
+//     if (request.method === 'post') {
+//
+//         if (!request.payload.username ||
+//             !request.payload.password) {
+//
+//             message = 'Missing username or password';
+//         }
+//         else {
+//             account = users[request.payload.username];
+//             if (!account ||
+//                 account.password !== request.payload.password) {
+//
+//                 message = 'Invalid username or password';
+//             }
+//         }
+//     }
+//
+//     if (request.method === 'get' ||
+//         message) {
+//
+//         return reply('<html><head><title>Login page</title></head><body>'
+//             + (message ? '<h3>' + message + '</h3><br/>' : '')
+//             + '<form method="post" action="/login">'
+//             + 'Username: <input type="text" name="username"><br>'
+//             + 'Password: <input type="password" name="password"><br/>'
+//             + '<input type="submit" value="Login"></form></body></html>');
+//     }
+//
+//     const sid = String(++uuid);
+//     request.server.app.cache.set(sid, { account: account }, 0, (err) => {
+//
+//         if (err) {
+//             return reply(err);
+//         }
+//
+//         request.cookieAuth.set({ sid: sid });
+//         return reply.redirect('/');
+//     });
+// };
+//
+// const logout = function (request, reply) {
+//
+//     request.cookieAuth.clear();
+//     return reply.redirect('/');
+// };
+
 export default [
   {
     method: 'POST',
@@ -76,6 +131,13 @@ export default [
     method: 'POST',
     path: '/api/auth/logout',
     handler: handleLogout
+  },
+  {
+    method: 'GET`',
+    path: '/api/auth/testauthcookie',
+    handler: (request, response) => {
+      reply(request.auth.credentials)
+    }
   },
 
 ]
--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,9 @@ import api from './api'
 
 const server = new Hapi.Server()
 
+const HapiAuthCookiePassword = 'very_securly_secret_passsword_do_not_let_anyone_know'
+const HapiAuthCookie = 'sid-example'
+
 module.exports = (port, callback) => {
 
   server.connection({
@@ -23,6 +26,34 @@ module.exports = (port, callback) => {
       if (err) {
         throw err
       }
+
+      //
+      // hapi-auth-cookie
+      //
+      const cache = server.cache({ segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000 });
+      server.app.cache = cache;
+
+      server.auth.strategy('session', 'cookie', true, {
+        password: HapiAuthCookiePassword,
+        cookie: HapiAuthCookie,
+        // redirectTo: '/login',
+        isSecure: false,
+        validateFunc: function (request, session, callback) {
+
+          cache.get(session.sid, (err, cached) => {
+
+            if (err) {
+              return callback(err, false);
+            }
+
+            if (!cached) {
+              return callback(null, false);
+            }
+
+            return callback(null, true, cached.account);
+          });
+        }
+      });
 
       //
       // Handle static files
