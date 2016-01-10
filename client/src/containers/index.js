34a84d9ab8bf139015ba89e312f328b6003eee98
wootsaejao <wootsaejao@gmail.com>
2016-01-10 16:37:11 +0700

    cleanup

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,7 +1,8 @@
 export Root from './Root/Root'
 export App from './App/App'
+export Products from './Products/Products'
+
 export Home from './Home'
 export Dashboard from './Dashboard'
 export Login from './Login'
 export Logout from './Logout'
-export Products from './Products/Products'
