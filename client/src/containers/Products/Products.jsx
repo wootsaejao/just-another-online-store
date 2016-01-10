13bfafe2f0bdcb3c15df1dd5a537ebab0d5c53c4
wootsaejao <wootsaejao@gmail.com>
2016-01-10 19:35:43 +0700

    apply layout from startbootstrap.com

--- client/src/containers/Products/Products.jsx
+++ client/src/containers/Products/Products.jsx
@@ -11,12 +11,59 @@ class Products extends Component {
   }
 
   render() {
-    return (
+
+    const sideBar = (
       <div>
-        products container
-        {this.props.products.map((item) =>
-          <div>{JSON.stringify(item.name)}</div>
-        )}
+        <p className="lead">Shop Name</p>
+        <div className="list-group">
+          <a href="#" className="list-group-item">Category 1</a>
+          <a href="#" className="list-group-item">Category 2</a>
+          <a href="#" className="list-group-item">Category 3</a>
+        </div>
+      </div>
+    )
+
+    const productBox = (
+      <div className="col-sm-4 col-lg-4 col-md-4">
+        <div className="thumbnail">
+          <img src="http://placehold.it/320x150" alt="" />
+            <div className="caption">
+              <h4 className="pull-right">$24.99</h4>
+              <h4><a href="#">First Product</a>
+            </h4>
+            <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+          </div>
+          <div className="ratings">
+            <p className="pull-right">15 reviews</p>
+            <p>
+              <span className="glyphicon glyphicon-star"></span>
+              <span className="glyphicon glyphicon-star"></span>
+              <span className="glyphicon glyphicon-star"></span>
+              <span className="glyphicon glyphicon-star"></span>
+              <span className="glyphicon glyphicon-star"></span>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+
+    return (
+      <div className="container">
+        <div className="row">
+
+          <div className="col-md-3">
+            {sideBar}
+          </div>
+          
+          <div className="col-md-9">
+            <div className="row">
+              {this.props.products.map((item) =>
+                productBox
+              )}
+            </div>
+          </div>
+
+        </div>
       </div>
     )
   }
