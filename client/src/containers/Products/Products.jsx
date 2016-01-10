4d76721a19d1d470eb94acbbcb5b6d876ba411ed
wootsaejao <wootsaejao@gmail.com>
2016-01-10 16:18:04 +0700

    product page

--- client/src/containers/Products/Products.jsx
+++ client/src/containers/Products/Products.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux';
 
-export default class Products extends Component {
+import * as ProductActions from '../../actions/ProductActions'
+
+class Products extends Component {
+
+  componentWillMount = () => {
+    this.props.actions.getProducts()
+  }
+
   render() {
     return (
       <div>
         products container
+        {this.props.products.map((item) =>
+          <div>{JSON.stringify(item.name)}</div>
+        )}
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    products: state._product.products
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(ProductActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products)
